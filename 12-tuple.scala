f6c8b7995635574fd527fa63d1809e437ceee4bf
Aliaksei Belablotski <v-abelablotski@expedia.com>
2015-09-11 13:35:05 -0700

    Tuples, strings, options

--- 12-tuple.scala
+++ 12-tuple.scala
@@ -1,3 +1,5 @@
+// Tuples in Scala
+
 val t1 = (1,2,3)
 
 
@@ -9,3 +11,17 @@ val t2 = 1 -> 2
 println(t2._1)
 println(t2.productElement(0))
 println(t2.productArity)
+
+
+val listOfTuples = List(1 -> 2, 3 -> 4, 5 -> 6, 7 -> 8)
+val r = listOfTuples map { case (x, y) => x + y }
+println(r)
+
+
+val listOfTuples2 = List("k1" -> "v1", "k2" -> "v2", "k3" -> "v3")
+listOfTuples2.foreach( 
+	_ match {
+		case ("k1", v) => println(s"Key one with value $v")
+		case (k, v) => println(s"Unknown key $k with value $v")
+	})
+	
---
+++ 52-Option.scala
@@ -0,0 +1,16 @@
+/*
+Trait Option:
+	isDefined
+	get
+	getorElse
+
+Subclasses:
+	Some
+	None
+*/
+
+
+match {
+	case Some(x) =>
+	case None =>
+}
