c84efd880630edf8c88a1381d92cbc723f761ac4
Aliaksei Belablotski <v-abelablotski@expedia.com>
2015-10-26 11:46:58 -0700

    Multiple match

--- 10-pattern_matching-default.scala
+++ 10-pattern_matching-default.scala
@@ -1,5 +1,13 @@
+/*
 // Without care about actual value:
 case _ => println("Default")
 
 // Need to analyze default value:
 case default => println("Default: " + default)
+*/
+
+val cmd = "b"
+cmd match {
+	case "a" | "e" | "i" | "o" | "u" => println("vowel")
+	case default => println(s"consonant ($default)")
+}
